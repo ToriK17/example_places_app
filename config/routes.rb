dc8e9dff54991ba3499f26aebb12bb7d64b79cd6
ToriK17 <vfcad@protonmail.com>
2019-10-09 14:18:23 -0700

    adds authentication

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
+    post "/users" => "users#create"
+    post "/sessions" => "sessions#create"
     get "/places" => "places#index"
     post "/places" => "places#create"
     get "/places/:id" => "places#show"
